c7486b5eeff287b79e6c662ce8d224d6ca6bceb4
Chuck LU <chucklu2015@outlook.com>
2023-01-31 18:08:21 +0800

    add MemCalculator

---
+++ Chapter2/LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter2.LogAn;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class MemCalculatorTests
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calculator = new MemCalculator();
+            int lastSum = calculator.Sum();
+            Assert.AreEqual(0, lastSum);
+        }
+    }
+}
