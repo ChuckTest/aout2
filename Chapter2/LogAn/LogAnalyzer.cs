913ceb7345b05b74183d44095d8199440a1e3d1e
Chuck LU <chucklu2015@outlook.com>
2023-01-31 15:39:28 +0800

    make lower case test pass

--- Chapter2/LogAn/LogAnalyzer.cs
+++ Chapter2/LogAn/LogAnalyzer.cs
@@ -1,10 +1,12 @@
-﻿namespace Chapter2.LogAn
+﻿using System;
+
+namespace Chapter2.LogAn
 {
     public class LogAnalyzer
     {
         public bool IsValidLogFileName(string fileName)
         {
-            if (!fileName.EndsWith(".SLF"))
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
             {
                 return false;
             }
