c1eb9bdd6b876df06180180aa8944596cdb07b2d
Chuck LU <chucklu2015@outlook.com>
2023-02-06 14:16:21 +0800

    use NSubstitute to mock

---
+++ Chapter5/Chapter5.LogAn.UnitTests/Listing5-3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Chapter5.LogAn.UnitTests
+{
+    [TestFixture]
+    public class Listing5_3
+    {
+        [Test]
+        public void Analyze_TooShortFileName_CallLogger()
+        {
+            ILogger logger = Substitute.For<ILogger>();//create a mock object that you'll assert against at the end of the test
+            LogAnalyzer analyzer = new LogAnalyzer(logger);
+            analyzer.MinNameLength = 6;
+
+            analyzer.Analyze("a.txt");
+
+            logger.Received().LoggerError("Filename too short: a.txt");//sets expectation using NSub's API
+        }
+    }
+}
