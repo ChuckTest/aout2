c169b9a5b09d73434c186881b91bc49ef690febc
Chuck LU <chucklu2015@outlook.com>
2023-02-06 18:06:46 +0800

    a test that uses handwritten mocks and stubs

--- Chapter5/Chapter5.LogAn.UnitTests/Listing5-5.cs
+++ Chapter5/Chapter5.LogAn.UnitTests/Listing5-5.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 
 namespace Chapter5.LogAn.UnitTests
 {
-    public class FakeWebService : IWebService
+    public class FakeWebService : IWebService//The fake web service you'll use as a mock
     {
         public string MessageToWebService;
 
@@ -17,7 +17,7 @@ namespace Chapter5.LogAn.UnitTests
         }
     }
 
-    public class FakeLogger2 : ILogger
+    public class FakeLogger2 : ILogger//The fake logger you'll use as a stub
     {
         public Exception WillThrow = null;
         public string LoggerGotMessage = null;
@@ -38,6 +38,18 @@ namespace Chapter5.LogAn.UnitTests
         [Test]
         public void Analyze_LoggerThrows_CallWebService()
         {
+            FakeWebService mockWebService = new FakeWebService();
+
+            FakeLogger2 stubLogger = new FakeLogger2();
+            stubLogger.WillThrow = new Exception("fake exception");
+
+            var analyzer = new LogAnalyzer2(stubLogger, mockWebService);
+            analyzer.MinNameLength = 8;
+
+            string tooShortFileName = "abc.ext";
+            analyzer.Analyze(tooShortFileName);
+
+            Assert.That(mockWebService.MessageToWebService, Does.Contain("fake exception"));
 
         }
     }
