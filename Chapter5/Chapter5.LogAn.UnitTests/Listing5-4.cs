7ab69492d58651365f3ae21389df97cd78779d9e
Chuck LU <chucklu2015@outlook.com>
2023-02-06 16:42:22 +0800

    Returning a value from a fake object

---
+++ Chapter5/Chapter5.LogAn.UnitTests/Listing5-4.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace Chapter5.LogAn.UnitTests
+{
+    [TestFixture]
+    public class Listing5_4
+    {
+        //Returning a value from a fake object
+        [Test]
+        public void Returns_ByDefault_WorksForHardCodeArgument()
+        {
+            IFileNameRules fakeRules = Substitute.For<IFileNameRules>();
+
+            fakeRules.IsValidLogFileName("strict.txt").Returns(true);//forces method call to return fake value
+
+            Assert.IsTrue(fakeRules.IsValidLogFileName("strict.txt"));
+        }
+    }
+}
