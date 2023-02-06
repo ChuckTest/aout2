a19a659518880ef18efcd2772ce2f74882672f7a
Chuck LU <chucklu2015@outlook.com>
2023-02-06 13:39:30 +0800

    add basic code for chapter 5

---
+++ Chapter5/Chapter5.LogAn/LogAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿namespace Chapter5.LogAn
+{
+    public class LogAnalyzer
+    {
+        private ILogger _logger;
+
+        public LogAnalyzer(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public int MinNameLength { get; set; }
+
+        public void Analyze(string fileName)
+        {
+            if (fileName.Length < MinNameLength)
+            {
+                _logger.LoggerError($"File name too short: {fileName}");
+            }
+        }
+    }
+}
