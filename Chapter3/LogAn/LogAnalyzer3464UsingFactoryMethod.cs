fd06dba676f359fe88ca07bfdae2f84d9f759a5a
Chuck LU <chucklu2015@outlook.com>
2023-03-15 16:41:11 +0800

    FAKE METHOD—USE A LOCAL FACTORY METHOD (EXTRACT AND OVERRIDE)

---
+++ Chapter3/LogAn/LogAnalyzer3464UsingFactoryMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    public class LogAnalyzer3464UsingFactoryMethod
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            return GetManager().IsValid(fileName);//Uses virtual GetManager() method
+        }
+
+        protected virtual IExtensionManager GetManager()
+        {
+            return new FileExtensionManager();//Returns hardcoded value
+        }
+    }
+}
