f042d3b0a62cb7a2bac60ecc488e10cb54fc3d46
Chuck LU <chucklu2015@outlook.com>
2023-03-15 17:18:24 +0800

    inherit the class under test, and override the dependency

---
+++ Chapter3/LogAn.UnitTests/LogAnalyzer3464Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzer3464Tests
+    {
+        [Test]
+        public void OverrideTest()
+        {
+            FakeExtensionManager stub = new FakeExtensionManager();
+            stub.WillBeValid = true;
+
+            //Creates instance of class derived from class under test
+            TestableLogAnalyzer logAnalyzer = new TestableLogAnalyzer(stub);
+            bool result = logAnalyzer.IsValidLogFileName("file.ext");
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Chapter3/LogAn.UnitTests/TestableLogAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.UnitTests
+{
+    internal class TestableLogAnalyzer : LogAnalyzer3464UsingFactoryMethod
+    {
+        public TestableLogAnalyzer(IExtensionManager mgr)
+        {
+            Manager = mgr;
+        }
+
+        public IExtensionManager Manager;
+        protected override IExtensionManager GetManager()
+        {
+            return Manager;
+        }
+    }
+}
