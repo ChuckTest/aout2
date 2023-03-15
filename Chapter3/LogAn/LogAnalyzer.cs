6bdb3d8e56d8fe4c9b4e5176157dfdf25011caa7
Chuck LU <chucklu2015@outlook.com>
2023-03-15 14:50:18 +0800

    introduce the problem the stub can resolve

---
+++ Chapter3/LogAn/LogAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+    //3.2 Identifying a filesystem dependency in LogAn
+    public class LogAnalyzer
+    {
+        // let’s assume that the allowed filenames are stored somewhere on disk as a configuration setting for the application
+        //: the code has some dependency on an external resource, which might break the test even though the code’s logic is perfectly valid
+        public bool IsValidLogFileName(string fileName)
+        {
+            //read through the configuration file
+            //return true if configuration says extension is supported.
+        }
+    }
+}
